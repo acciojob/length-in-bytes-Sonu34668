f664528c427f0ffece753404fb42d1e8ed337d6a
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-08-21 13:54:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// const byteSize = (str) => {
-//   // write your code here
-// };
-
-// // Do not change the code below
-// const str = prompt("Enter some string.");
-// alert(byteSize(str));
-
-
 const byteSize = (str) => {
   // write your code here
 	if(str.trim()==0) return 0;
